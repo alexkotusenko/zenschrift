29bd026c6a130fae77765aedba16babf2e22d63c
Alex K. <alexkotusenko@gmail.com>
2026-01-10 21:30:14 -0500

    Refactored a bit

--- zenschrift.lua
+++ zenschrift.lua
@@ -2,11 +2,33 @@
 
 local M = {}
 
-local zen_enabled = false
+-- Default configuration
+-- Specify an override for any of these options when configuring.
+-- For example:
+-- ```
+-- require("zenschrift.nvim").setup({
+--   number = true, -- to show line numbers
+--   -- if nothing else is passed, 
+--   -- you will get defaults everywhere else 
+-- })
+-- ```
+M.config = {
+  laststatus = 0,
+  cmdheight = 0,
+  showmode = false,
+  number = false,
+  relativenumber = false,
+  signcolumn = "no",
+  ruler = false,
+  showcmd = false,
+  foldcolumn = "2",
+  fillchars = "fold: ,eob: ",
+}
 
+local zen_enabled = false
 local saved = {}
 
-local function save(opts)
+local function apply(opts)
   for k, v in pairs(opts) do
     saved[k] = vim.o[k]
     vim.o[k] = v
@@ -22,21 +44,16 @@ end
 
 function M.toggle()
   if not zen_enabled then
-    save({
-      laststatus = 0,
-      cmdheight = 0,
-      showmode = false,
-      number = false,
-      relativenumber = false,
-      signcolumn = "no",
-      ruler = false,
-      showcmd = false,
-    })
+    apply(M.config)
     zen_enabled = true
   else
     restore()
     zen_enabled = false
   end
+end
+
+function M.setup(opts)
+  M.config = vim.tbl_deep_extend("force", M.config, opts or {})
 end
 
 return M
